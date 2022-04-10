1786d7ab74325d0e95743e91103360f0674c607b
Gopal Ramesh Dahale <dahalegopal27@gmail.com>
2022-04-10 16:43:33 +0530

    Added SAES21

--- SAES/saes_test.py
+++ SAES/saes_test.py
@@ -1,8 +1,8 @@
 from saes import SAES
 import numpy as np
 
-msg = int('0b1101011100101000', 2)
-key = int('0b0100101011110101', 2)
+msg = int('0b0110111101101011', 2)
+key = int('0b1010011100111011', 2)
 res = int('0b0010010011101100', 2)
 
 print('msg:', msg)
@@ -10,7 +10,7 @@ print('key:', key)
 
 saes = SAES(key)
 ans = saes.encrpyt(msg, 2)
-print(res, ans)
+print(res, bin(ans)[2:].zfill(16))
 print(saes.decrypt(ans, 2))
 # ans = saes.decrpyt(ans, 2)
 print(ans)
